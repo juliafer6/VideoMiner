8a9006a5835d1c6f10f7927b6d03c4773fc7f0b8
Paula-Arias11 <pauarijim@alum.us.es>
2026-05-09 19:11:30 +0200

    Comentado apiKey

--- src/main/java/aiss/videominer/security/ApiKeyInterceptor.java
+++ src/main/java/aiss/videominer/security/ApiKeyInterceptor.java
@@ -8,17 +8,19 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
 public class ApiKeyInterceptor implements HandlerInterceptor {
-
+    // Sacamos la clave desde el archivo application.properties
     @Value("${videominer.api.key}")
     private String apiKey;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
+        // Buscamos en las cabeceras de la petición el campo "X-API-KEY"
         String requestApiKey = request.getHeader("X-API-KEY");
-
+        // Si la clave existe y coincide con la nuestra, le dejamos pasar (true)
         if ( requestApiKey !=null && requestApiKey.equals(apiKey)){
             return true;
         } else {
+            // Si no, paramos la petición y devolvemos error 401
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             response.getWriter().write("Error 401:The user is not logged in (API Key no valida o ausente)");
             return false;
--- src/main/java/aiss/videominer/security/webConfig.java
+++ src/main/java/aiss/videominer/security/webConfig.java
@@ -8,13 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class webConfig implements WebMvcConfigurer {
+
+    // Traemos el interceptor que creamos antes
     @Autowired
     private ApiKeyInterceptor apiKeyInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry){
+        // Aquí registramos el interceptor
         registry.addInterceptor(apiKeyInterceptor)
-                .addPathPatterns("/videominer/**")
-                .excludePathPatterns("/h2-ui/**");
+                .addPathPatterns("/videominer/**")// Se aplica a cualquier ruta que empiece por /videominer/
+                .excludePathPatterns("/h2-ui/**");// Dejamos libre el acceso a la consola de la base de datos H2
     }
 }
